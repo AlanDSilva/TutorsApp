ea5c1a19626b02ee091b849bbc799da006a3a811
Alan <dasilva.alan@outlook.com>
2021-05-17 16:11:36 +0300

    Initial Commit

---
+++ Tutors/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Tutors
+//
+//  Created by Alan on 17.5.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Tutors/TutorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TutorsApp.swift
+//  Tutors
+//
+//  Created by Alan on 17.5.2021.
+//
+
+import SwiftUI
+
+@main
+struct TutorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
