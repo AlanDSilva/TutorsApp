f674ccd3fe3c03674e8be8951e7b076c8f0afa00
Alan <dasilva.alan@outlook.com>
2021-05-20 17:22:35 +0300

    added pods and connected to firebase

--- Tutors/App/TutorsApp.swift
+++ Tutors/App/TutorsApp.swift
@@ -6,9 +6,15 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct TutorsApp: App {
+    
+    init() {
+        FirebaseApp.configure()
+    }
+    
     var body: some Scene {
         WindowGroup {
             MainView()
